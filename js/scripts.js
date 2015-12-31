29cf4dd1e559401f3f68011ca046129ca49116b2
woni <woni@mhand.net>
2015-12-31 19:42:02 +0900

    * 전체 스타일 재 적용.

--- js/scripts.js
+++ js/scripts.js
@@ -105,7 +105,7 @@ function calcScroll() {
     $('section.navigation').addClass('fixed');
     $('header').css({
       "border-bottom": "none",
-      "padding": "35px 0"
+      "padding": "23px 0"
     });
     $('header .member-actions').css({
       "top": "26px",
